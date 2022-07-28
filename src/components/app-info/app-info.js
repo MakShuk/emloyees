086e3111d1e99f542f8f8d98f18eb13236d746e5
Maksim <makshuk@bk.ru>
2022-07-28 10:30:15 +0300

    add components

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,14 @@
+import './app-info.css';
+
+
+const AppInfo  = () => {
+ return (
+  <div className="app-info">
+    <h1>Учет сотрудников N</h1>
+    <h2>Общее число сотрудников: __</h2>
+    <h2>Пркмию получат: __</h2>
+  </div>
+ )
+}
+
+export default AppInfo;
