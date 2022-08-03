65f36fe4d838a9bfbb2820b05dc9eacec173abdc
Maksim <makshuk@bk.ru>
2022-08-03 11:15:57 +0300

    fix

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/serch-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,28 +8,60 @@ import EmployeesAddForm from '../employers-add-form/employees-add-form';
 
 import './app.css';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: 'Maks', salary: 800, increase: false, id: 1 },
+        { name: 'Ania', salary: 200, increase: false, id: 2 },
+        { name: 'Den', salary: 1800, increase: false, id: 3 },
+      ],
+    };
+    this.maxId = 4;
+    this.addItem = this.addItem.bind(this);
+  }
 
-function App() {
+  deleteItem = (id) => {
+    this.setState(({ data }) => {
+      return {
+        data: data.filter((item) => item.id !== id),
+      };
+    });
+  };
 
-  const data = [
-    { name: 'Maks S.', salary: '200', increase: false },
-    { name: 'Alex F.', salary: '100', increase: true },
-    { name: 'Ana  S.', salary: '50', increase: false },
-    { name: 'Omna  k.', salary: '150', increase: true },
-  ];
+  addItem(name, salary){
+       const newItem = {
+         name,
+         salary,
+         increase: false,
+         rise: false,
+         id: this.maxId++,
+       };
+       this.setState(({ data }) => {
+         const newArr = [...data, newItem];
+         return {
+           data: newArr,
+         };
+       });
+  };
+  
 
-
-  return (
-    <div className="app">
-      <AppInfo />
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
+  render() {
+    return (
+      <div className="app">
+        <AppInfo />
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+        </div>
+        <EmployeesList data={this.state.data} onDelete={this.deleteItem} />
+        <EmployeesAddForm
+         onAdd={this.addItem}
+        />
       </div>
-      <EmployeesList  data={data}/>
-      <EmployeesAddForm/>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/employers-list/employees-list.js
+++ src/components/employers-list/employees-list.js
@@ -2,11 +2,21 @@ import EmployeesListItem from '../employers-list-item/employees-list-item';
 
 import './employees-list.css';
 
-const EmployeesList = ({ data }) => {
+const EmployeesList = ({ data, onDelete }) => {
   const elements = data.map((item) => {
-    return <EmployeesListItem {...item} />;
+    const { id, ...itemProps } = item;
+    return (
+      <EmployeesListItem
+        key={id}
+        {...itemProps}
+        onDelete={() => {
+          onDelete(id)
+        }}
+      />
+    );
   });
   return <ul className="app-list list-group">{elements}</ul>;
 };
 
 export default EmployeesList;
+     
